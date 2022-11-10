a06a8584035c5c187a9f6fbef9cdac71e07da768
牛心怡 <8402055+nxy8285@user.noreply.gitee.com>
2022-11-10 16:50:26 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,18 @@
 # Use https://hub.docker.com/r/tiangolo/uwsgi-nginx-flask/
 # hello world demo
-FROM tiangolo/uwsgi-nginx-flask:python3.8
+# FROM tiangolo/uwsgi-nginx-flask:python3.8
 
-COPY ./app /app
+# COPY ./app /app
+FROM python:3.6
+
+LABEL maintainer="xxx <xx@qq.com>"
+
+COPY . /detectweb
+
+WORKDIR /detectweb
+
+RUN pip install -r requirements.txt -i https://mirror.baidu.com/pypi/simple && pip install gunicorn&&chmod 755 run_server.sh
+
+EXPOSE 8080
+
+# ENTRYPOINT [ "./run_server.sh" ]
--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -20,16 +20,16 @@ from django.db import models
 import numpy as np
 import cv2
 from PIL import Image, ImageDraw, ImageFont
-import PIL
+# import PIL
 import matplotlib.pyplot as plt
 import os
 import shutil
-from numpy.core.records import array
-from numpy.core.shape_base import block
-import time
-from collections import Counter
+# from numpy.core.records import array
+# from numpy.core.shape_base import block
+# import time
+# from collections import Counter
 import cnn
-from glob import glob
+# from glob import glob
 
 # %%
 # 整幅图片的Y轴投影
@@ -148,36 +148,36 @@ def cut_img(img, mark_boxs, is_square = False):
     return img_items
 
 # 展示投影图
-def show_shadow(arr, direction = 'x'):
+# def show_shadow(arr, direction = 'x'):
 
-    a_max = max(arr)
-    if direction == 'x': # x轴方向的投影
-        a_shadow = np.zeros((a_max, len(arr)), dtype=int)
-        for i in range(0,len(arr)):
-            if arr[i] == 0:
-                continue
-            for j in range(0, arr[i]):
-                a_shadow[j][i] = 255
-    elif direction == 'y': # y轴方向的投影
-        a_shadow = np.zeros((len(arr),a_max), dtype=int)
-        for i in range(0,len(arr)):
-            if arr[i] == 0:
-                continue
-            for j in range(0, arr[i]):
-                a_shadow[i][j] = 255
+#     a_max = max(arr)
+    # if direction == 'x': # x轴方向的投影
+    #     a_shadow = np.zeros((a_max, len(arr)), dtype=int)
+    #     for i in range(0,len(arr)):
+    #         if arr[i] == 0:
+    #             continue
+    #         for j in range(0, arr[i]):
+    #             a_shadow[j][i] = 255
+    # elif direction == 'y': # y轴方向的投影
+    #     a_shadow = np.zeros((len(arr),a_max), dtype=int)
+    #     for i in range(0,len(arr)):
+    #         if arr[i] == 0:
+    #             continue
+    #         for j in range(0, arr[i]):
+    #             a_shadow[i][j] = 255
 
-    img_show_array(a_shadow)
+    # img_show_array(a_shadow)
 
 # 展示图片，路径展示方式
-def img_show_path(img_path):
-    pil_im = Image.open(img_path)
-    plt.imshow(pil_im)
-    plt.show()
+# def img_show_path(img_path):
+#     pil_im = Image.open(img_path)
+#     plt.imshow(pil_im)
+    # plt.show()
 
 # 展示图片，数组展示方式
-def img_show_array(a):
-    plt.imshow(a)
-    plt.show()
+# def img_show_array(a):
+#     plt.imshow(a)
+#     plt.show()
 
 # 保存图片
 def save_imgs(dir_name, imgs):
